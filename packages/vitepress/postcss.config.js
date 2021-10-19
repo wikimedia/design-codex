e04a7f45b21c246f73d3ce3139501699885cd753
Anne Tomasevich <atomasevich@wikimedia.org>
2021-10-19 18:21:59 -0400

    build: Add postcss-nested and postcss-mixins plugins
    The postcss-preset-env plugin has no feature similar to mixins
    and sticks to the CSS spec for nesting, which doesn't support
    parent selector concatenation. We need both of these features to
    effectively share modular styles.
    
    Bug: T293527
    Change-Id: I368bb3896e59f51ac23bbd949ccb79eaa3b556e8

--- packages/vitepress/postcss.config.js
+++ packages/vitepress/postcss.config.js
@@ -1,9 +1,18 @@
 const postcssPresetEnv = require( 'postcss-preset-env' );
+const postcssNested = require( 'postcss-nested' );
+const postcssMixins = require( 'postcss-mixins' );
 
 module.exports = {
 	plugins: [
+		postcssNested(),
+		postcssMixins(),
 		postcssPresetEnv( {
-			stage: 0
+			stage: 0,
+			features: {
+				// postcss-preset-env uses the postcss-nesting plugin, but we want nesting rules
+				// from postcss-nested instead (particularly for parent selector concatenation).
+				'nesting-rules': false
+			}
 		} )
 	]
 };
--- packages/vue-components/postcss.config.js
+++ packages/vue-components/postcss.config.js
@@ -1,11 +1,20 @@
 const postcssPresetEnv = require( 'postcss-preset-env' );
+const postcssNested = require( 'postcss-nested' );
+const postcssMixins = require( 'postcss-mixins' );
 const postcssRemoveRoot = require( 'postcss-remove-root' );
 
 module.exports = {
 	plugins: [
+		postcssNested(),
+		postcssMixins(),
 		postcssPresetEnv( {
 			stage: 0,
-			preserve: false
+			preserve: false,
+			features: {
+				// postcss-preset-env uses the postcss-nesting plugin, but we want nesting rules
+				// from postcss-nested instead (particularly for parent selector concatenation).
+				'nesting-rules': false
+			}
 		} ),
 		postcssRemoveRoot()
 	]
