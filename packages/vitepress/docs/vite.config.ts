9f9db2eaf57b39044e0ee419e332285c05647e89
Eric Gardner <egardner@wikimedia.org>
2021-11-08 15:03:25 -0800

    docs: Explicitly allow VitePress dev server to serve workspace files
    When run locally, the Vite dev-server for the docs site issued warnings
    about FS access for component source-code (which lives in a neighboring
    workspace). In a future version of Vite this behavior might be
    prohibited by default (although nothing breaks currently).
    
    This change fixes the warnings by explicitly allowing neighboring
    workspace files to be served during development. We need this to
    auto-generate source code for the component demo pages.
    
    See: https://vitejs.dev/config/#server-fs-allow
    Change-Id: Ice5c54700eabd4506aeb1dca0c775ad45af48d09

--- packages/vitepress/docs/vite.config.ts
+++ packages/vitepress/docs/vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
+	server: {
+		/**
+		 * Explicitly allow dev server to serve files from other workspaces.
+		 */
+		fs: { allow: [ '../..' ] }
+	},
+
 	build: {
 		minify: true
 	}
