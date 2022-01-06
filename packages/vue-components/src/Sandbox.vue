46be026d3a18c41b8b0ccfb0d3cfe44972b5dab8
Roan Kattouw <roan.kattouw@gmail.com>
2022-01-06 12:01:53 -0800

    Sandbox: Make sure all components are represented
    Add examples of missing components, and also demo icons inside buttons.
    Also make the demo fixed-width, so that things that are 100% width (like
    text inputs) don't look too ugly.
    
    Change-Id: I32cf536f4619d9e835da23baf143a12f7ea50303

--- packages/vue-components/src/Sandbox.vue
+++ packages/vue-components/src/Sandbox.vue
@@ -4,24 +4,48 @@
 			<h1>Codex Demo</h1>
 		</header>
 
-		<main>
+		<main class="sandbox-main">
 			<h2>Select</h2>
-
 			<cdx-select
 				v-model="selection"
 				:options="options"
-				:default-label="'Choose an option'"
-				:disabled="false"
+				default-label="Choose an option"
 			/>
+			<p>Selected value: {{ selection || '(none)' }}</p>
+
+			<h2>Radios</h2>
+			<cdx-radio
+				v-for="option in options"
+				:key="option.value"
+				v-model="selection"
+				:input-value="option.value"
+			>
+				{{ option.label }}
+			</cdx-radio>
+			<p>Selected value: {{ selection || '(none)' }}</p>
+
+			<h2>Text input</h2>
+			Selected value: <cdx-text-input v-model="selection" clearable />
+
+			<h2>Checkboxes</h2>
+			<cdx-checkbox
+				v-for="option in options"
+				:key="option.value"
+				v-model="multiSelection"
+				:input-value="option.value"
+			>
+				{{ option.label }}
+			</cdx-checkbox>
+			<p>Selected values: {{ multiSelection.join( ', ' ) || '(none)' }}</p>
 
 			<h2>Icons</h2>
 			<p dir="rtl">
 				<cdx-icon :icon="cdxIconArrowNext" /> next
 			</p>
-			<p lang="nl">
+			<p lang="nl" dir="ltr">
 				<cdx-icon :icon="cdxIconBold" /> Bold
 			</p>
-			<h2>Codex Button</h2>
+			<h2>Button</h2>
 			<table>
 				<thead>
 					<tr>
@@ -40,6 +64,7 @@
 								:type="buttonType"
 								:action="buttonAction"
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 							<br><br>
@@ -48,6 +73,7 @@
 								:type="buttonType"
 								:action="buttonAction"
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 						</td>
@@ -57,6 +83,7 @@
 							<cdx-button
 								:action="buttonAction"
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 							<br><br>
@@ -64,6 +91,7 @@
 								disabled
 								:action="buttonAction"
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 						</td>
@@ -76,6 +104,7 @@
 							<cdx-button
 								:type="buttonType"
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 							<br><br>
@@ -84,6 +113,7 @@
 								:type="buttonType"
 								@click="onClick"
 							>
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 						</td>
@@ -92,12 +122,14 @@
 						<td>
 							<cdx-button
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 							<br><br>
 							<cdx-button
 								disabled
 								@click="onClick">
+								<cdx-icon :icon="cdxIconTrash" />
 								Button
 							</cdx-button>
 						</td>
@@ -109,8 +141,8 @@
 </template>
 
 <script lang="ts" setup>
-import { CdxButton, CdxIcon, CdxSelect } from './lib';
-import { cdxIconArrowNext, cdxIconBold } from 'icons';
+import { CdxButton, CdxCheckbox, CdxIcon, CdxRadio, CdxSelect, CdxTextInput } from './lib';
+import { cdxIconArrowNext, cdxIconBold, cdxIconTrash } from 'icons';
 import { ButtonActions, ButtonTypes } from './constants';
 import { MenuOption } from './types';
 import { ref } from 'vue';
@@ -122,6 +154,7 @@ const options: MenuOption[] = [
 ];
 
 const selection = ref<string>();
+const multiSelection = ref<string[]>( [] );
 
 function onClick( e: Event ) {
 	// eslint-disable-next-line no-console
@@ -130,6 +163,10 @@ function onClick( e: Event ) {
 </script>
 
 <style scoped>
+.sandbox-main {
+	max-width: 500px;
+}
+
 td {
 	/* stylelint-disable-next-line unit-disallowed-list */
 	padding: 1.5rem;
