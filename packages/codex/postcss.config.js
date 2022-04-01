7da32c148fb46a983adcb14ed8abc5d4f0b5b65b
Roan Kattouw <roan.kattouw@gmail.com>
2022-04-01 15:47:21 -0700

    build: Check .js files with TypeScript in the Codex package
    Change-Id: I06e9506383ca5d99a51a398a8599882a1fe1503b

--- packages/codex/postcss.config.js
+++ packages/codex/postcss.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable jsdoc/valid-types */
+
 const autoprefixer = require( 'autoprefixer' );
 const postcssRtlcss = require( 'postcss-rtlcss' );
 
 module.exports = {
+	/** @type {import('postcss').AcceptedPlugin[]} */
 	plugins: [
 		postcssRtlcss( {
 			useCalc: true,
