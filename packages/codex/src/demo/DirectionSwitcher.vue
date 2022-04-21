3e0e5e0b7be09ffe8dd0d8f247fdaa2ccb193b24
DannyS712 <dannys712.wiki@gmail.com>
2022-04-21 19:34:23 +0000

    DirectionSwitcher: use ToggleButton for direction options
    Instead of normal buttons, use toggle buttons. Like before,
    clicking on the button for the direction not currently used
    switches the direction, while clicking the button for the
    current direction does not change anything.
    
    Bug: T305278
    Change-Id: I9c6898942a7beb5ba486184a535b9b54b6146f1f

--- packages/codex/src/demo/DirectionSwitcher.vue
+++ packages/codex/src/demo/DirectionSwitcher.vue
@@ -1,24 +1,26 @@
 <template>
-	<cdx-button
-		v-for="direction in directions"
-		:key="direction.value"
-		type="primary"
-		:action="modelValue === direction.value ? 'progressive' : 'default'"
-		@click="$emit( 'update:modelValue', direction.value )"
-	>
-		{{ direction.label }}
-	</cdx-button>
+	<!--
+		Not using v-model so that
+		a) values can be synced (with inverse values for the two buttons)
+		b) update event can be emitted
+	-->
+	<cdx-toggle-button :model-value="isLTR" @update:model-value="useLTR">
+		LTR
+	</cdx-toggle-button>
+	<cdx-toggle-button :model-value="isRTL" @update:model-value="useRTL">
+		RTL
+	</cdx-toggle-button>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import CdxButton from '../components/button/Button.vue';
+import { computed, defineComponent, PropType } from 'vue';
+import CdxToggleButton from '../components/toggle-button/ToggleButton.vue';
 import { HTMLDirection } from '../types';
 
 // TODO this component will be superfluous once we have a ButtonGroup component
 export default defineComponent( {
 	components: {
-		CdxButton
+		CdxToggleButton
 	},
 	props: {
 		modelValue: {
@@ -29,11 +31,25 @@ export default defineComponent( {
 	emits: [
 		'update:modelValue'
 	],
-	data: () => ( {
-		directions: [
-			{ value: 'ltr', label: 'LTR' },
-			{ value: 'rtl', label: 'RTL' }
-		]
-	} )
+	setup( props, { emit } ) {
+		// State
+		const isLTR = computed( () => props.modelValue === 'ltr' );
+		const isRTL = computed( () => props.modelValue === 'rtl' );
+
+		// Toggle - if the currently-used direction is clicked again, it does nothing
+		const useLTR = () => {
+			emit( 'update:modelValue', 'ltr' );
+		};
+		const useRTL = () => {
+			emit( 'update:modelValue', 'rtl' );
+		};
+
+		return {
+			isLTR,
+			isRTL,
+			useLTR,
+			useRTL
+		};
+	}
 } );
 </script>
