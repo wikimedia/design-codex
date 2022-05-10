bf46a51d221e038ad021a69a5af2e65e0635237c
Roan Kattouw <roan.kattouw@gmail.com>
2022-05-10 12:20:08 -0700

    useIntersectionObserver: Don't observe elements before they're mounted
    Trying to observe an element before the component was mounted caused
    strange issues. When the element was visible at mount time, the observer
    would falsely tell us it wasn't visible, and we'd only get the right
    answer after the user scrolled the page or resized the window.
    
    By waiting to observe the element until the component is mounted, we do
    get correct information.
    
    Change-Id: Id59e143322b11e429ef4ff4799afbcde6e6f06ed

--- packages/codex/src/composables/useIntersectionObserver.ts
+++ packages/codex/src/composables/useIntersectionObserver.ts
@@ -4,8 +4,8 @@ import { ref, Ref, onMounted, onUnmounted, watch } from 'vue';
  * Sets up a basic IntersectionObserver which attaches to the provided
  * template ref when the component is mounted. Returns a boolean ref which
  * will update to match the current intersection status of the targeted
- * element. If IntersectionObserver is not available, the returned ref
- * will always be false.
+ * element (true if the element is in view, false if it's scrolled out of view).
+ * If IntersectionObserver is not available, the returned ref will always be false.
  *
  * @param templateRef
  * @param observerOptions
@@ -16,6 +16,7 @@ export default function useIntersectionObserver(
 	observerOptions: IntersectionObserverInit
 ): Ref<boolean> {
 	const intersectionRef = ref( false );
+	let mounted = false;
 
 	// Exit early if not running in a browser
 	if ( typeof window !== 'object' ) {
@@ -43,18 +44,25 @@ export default function useIntersectionObserver(
 	);
 
 	onMounted( () => {
+		mounted = true;
 		if ( templateRef.value ) {
 			observer.observe( templateRef.value );
 		}
 	} );
 
 	onUnmounted( () => {
+		mounted = false;
 		observer.disconnect();
 	} );
 
 	// If the templateRef changes to point to a different element, disconnect the observer from the
 	// old element and observe the new one instead
 	watch( templateRef, ( newElement ) => {
+		// Don't try to observe the element before the component has been mounted; doing that leads
+		// to strange bugs.
+		if ( !mounted ) {
+			return;
+		}
 		observer.disconnect();
 		// Reset the ref back to false, just like when we initialized it. If newElement is set, the
 		// observe callback will run and set the ref to true if newElement is visible.
