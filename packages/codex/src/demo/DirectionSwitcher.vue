9dc480581d7c5ec6fa3b510b7f7031f1300a77bf
Roan Kattouw <roan.kattouw@gmail.com>
2022-07-26 15:34:00 -0700

    DirectionSwitcher: Use ToggleButtonGroup now that it exists
    Change-Id: Ife05266d73a514f1061d5a12f872081c0101f91a

--- packages/codex/src/demo/DirectionSwitcher.vue
+++ packages/codex/src/demo/DirectionSwitcher.vue
@@ -1,26 +1,20 @@
 <template>
-	<!--
-		Not using v-model so that
-		a) values can be synced (with inverse values for the two buttons)
-		b) update event can be emitted
-	-->
-	<cdx-toggle-button :model-value="isLTR" @update:model-value="useLTR">
-		LTR
-	</cdx-toggle-button>
-	<cdx-toggle-button :model-value="isRTL" @update:model-value="useRTL">
-		RTL
-	</cdx-toggle-button>
+	<cdx-toggle-button-group
+		v-model="modelWrapper"
+		:buttons="buttons"
+		class="cdx-demo-direction-switcher"
+	/>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue';
-import CdxToggleButton from '../components/toggle-button/ToggleButton.vue';
-import { HTMLDirection } from '../types';
+import { defineComponent, toRef, PropType } from 'vue';
+import CdxToggleButtonGroup from '../components/toggle-button-group/ToggleButtonGroup.vue';
+import useModelWrapper from '../composables/useModelWrapper';
+import { ButtonGroupItem, HTMLDirection } from '../types';
 
-// TODO this component will be superfluous once we have a ButtonGroup component
 export default defineComponent( {
 	components: {
-		CdxToggleButton
+		CdxToggleButtonGroup
 	},
 	props: {
 		modelValue: {
@@ -32,24 +26,22 @@ export default defineComponent( {
 		'update:modelValue'
 	],
 	setup( props, { emit } ) {
-		// State
-		const isLTR = computed( () => props.modelValue === 'ltr' );
-		const isRTL = computed( () => props.modelValue === 'rtl' );
-
-		// Toggle - if the currently-used direction is clicked again, it does nothing
-		const useLTR = () => {
-			emit( 'update:modelValue', 'ltr' );
-		};
-		const useRTL = () => {
-			emit( 'update:modelValue', 'rtl' );
-		};
+		const buttons: ButtonGroupItem[] = [
+			{ value: 'ltr', label: 'LTR' },
+			{ value: 'rtl', label: 'RTL' }
+		];
+		const modelWrapper = useModelWrapper( toRef( props, 'modelValue' ), emit );
 
 		return {
-			isLTR,
-			isRTL,
-			useLTR,
-			useRTL
+			buttons,
+			modelWrapper
 		};
 	}
 } );
 </script>
+
+<style lang="less">
+.cdx-demo-direction-switcher {
+	display: inline-block;
+}
+</style>
