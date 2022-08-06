16e69748d909eb63a2dd1a003914b66ca45dfaeb
Roan Kattouw <roan.kattouw@gmail.com>
2022-08-05 19:00:30 -0700

    build: Publish the sandbox alongside Netlify deployment previews
    For a deployment preview published to
    123456--wikimedia-codex.netlify.app , this publishes the sandbox at
    123456--wikimedia-codex.netlify.app/sandbox .
    
    - Bifurcate the options in codex's Vite config
    - Add a build-sandbox NPM command to the codex package that runs the
      Vite build with mode='sandbox'
    - Have the branch-deploy command run that and copy the result into the
      Vitepress dist directory
    
    Bug: T314792
    Change-Id: I61e909c336df5a2bc7428875a50f50d7854379a4

--- packages/codex/vite.config.ts
+++ packages/codex/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import rtlcss from 'rtlcss';
 import postcssRtlcss from 'postcss-rtlcss';
@@ -8,7 +8,7 @@ import postcssBaseConfig from './postcss-base.config';
 // https://vitejs.dev/config/
 // Take an extra libName parameter so it can be customized in packages/codex-search/vite.config.ts
 export default defineConfig( ( { command, mode }, libName = 'codex' ) => {
-	// We run the build twice: first with --mode=rtl, then without.
+	// In library mode, we run the build twice: first with --mode=rtl, then without.
 	// The --mode=rtl build builds the RTL version of the stylesheet, flipped using rtlcss,
 	// and puts it at codex.style-rtl.css
 	const isRtlBuild = command === 'build' && mode === 'rtl';
@@ -20,9 +20,10 @@ export default defineConfig( ( { command, mode }, libName = 'codex' ) => {
 		postcssConfig.plugins.unshift( rtlcss( {
 			useCalc: true
 		} as rtlcss.ConfigOptions ) );
-	} else if ( command === 'serve' ) {
-		// In dev mode, use postcss-rtlcss instead, so that the direction switcher works
-		// Add it to the beginning of the plugins list, before autoprefixer, otherwise things break
+	} else if ( command === 'serve' || mode === 'sandbox' ) {
+		// For the sandbox (in dev mode or in build mode), use postcss-rtlcss instead, so that the
+		// direction switcher works. Add it to the beginning of the plugins list, before
+		// autoprefixer, otherwise things break.
 		postcssConfig.plugins.unshift( postcssRtlcss( {
 			useCalc: true,
 			processKeyFrames: true,
@@ -30,7 +31,8 @@ export default defineConfig( ( { command, mode }, libName = 'codex' ) => {
 		} ) );
 	}
 
-	return {
+	// Common config shared between the library build and the sandbox build
+	const commonConfig: Partial<UserConfig> = {
 		build: {
 			// The es2015 build target doesn't
 			// - include Edge 18, but we do support it in our Modern support browser group and it
@@ -40,7 +42,43 @@ export default defineConfig( ( { command, mode }, libName = 'codex' ) => {
 			//   minifier will output CSS that uses the 'inset:' property, which isn't supported in
 			//   Safari 14 and below.
 			target: [ 'es2015', 'edge18', 'safari11' ],
-			minify: true,
+			minify: true
+
+		},
+		css: {
+			// This type assertion is needed because Vite's types expect the stricter Plugin type
+			// from PostCSS here, but what we have is the more permissive AcceptedPlugin type
+			postcss: postcssConfig as { plugins: PostCSSPlugin[] }
+		},
+		plugins: [
+			vue()
+		]
+	};
+
+	if ( mode === 'sandbox' ) {
+		// Don't build the library, only build the sandbox (index.html)
+		return {
+			...commonConfig,
+
+			// Allow the sandbox to be published underneath the documentation website,
+			// under /sandbox. See also the Vitepress config in the codex-docs package
+			// for how CODEX_DOC_ROOT is used there.
+			base: `${process.env.CODEX_DOC_ROOT || ''}/sandbox/`,
+
+			build: {
+				...commonConfig.build,
+				outDir: 'dist/sandbox'
+			}
+		};
+	}
+
+	// Regular library build
+	return {
+		...commonConfig,
+
+		build: {
+			...commonConfig.build,
+
 			// Since we run Vite twice, don't empty the dist directory in between runs
 			// We do this in the build command in package.json instead.
 			emptyOutDir: false,
@@ -62,14 +100,6 @@ export default defineConfig( ( { command, mode }, libName = 'codex' ) => {
 
 				external: [ 'vue' ]
 			}
-		},
-		css: {
-			// This type assertion is needed because Vite's types expect the stricter Plugin type
-			// from PostCSS here, but what we have is the more permissive AcceptedPlugin type
-			postcss: postcssConfig as { plugins: PostCSSPlugin[] }
-		},
-		plugins: [
-			vue()
-		]
+		}
 	};
 } );
