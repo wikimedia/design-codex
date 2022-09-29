57dbdbda032463ca2aab218daef23cbd95c97c55
Roan Kattouw <roan.kattouw@gmail.com>
2022-09-28 17:49:50 -0700

    Link: Make selectors flexible
    The .cdx-mixin-link() mixin assumes that the CSS classes is-red-link and
    is-underlined are used for red/underlined links. To allow mixin users to
    use different selectors if they want, split the mixin into sub-mixins
    that can be used independently. The main mixin still exists and still
    works as before, but is now implemented using the sub-mixins.
    
    See the documentation comment in link.less and the usage in LinkDemo.vue
    for examples.
    
    Bug: T316895
    Change-Id: I72ef166563b629729b1eb08028d06e9ae8040bc1

--- packages/codex/src/demo/LinkDemo.vue
+++ packages/codex/src/demo/LinkDemo.vue
@@ -20,6 +20,18 @@
 		<h3>Icon Red link</h3>
 		<p>Lorem ipsum <a class="my-custom-link-class is-red-link" href="https://example.com"><cdx-icon :icon="cdxIconLink" />dolor sic amet</a></p>
 		<p>Lorem ipsum <a class="my-custom-link-class is-red-link is-underlined" href="https://example.com">dolor sic amet <cdx-icon :icon="cdxIconLinkExternal" /></a></p>
+
+		<h3>Links with custom CSS selectors</h3>
+		<div class="my-link-wrapper">
+			<p>Lorem ipsum <a href="https://www.example.com">regular link</a></p>
+			<p class="my-link-wrapper-with-underline">
+				Lorem ipsum <a href="https://www.example.com">underlined link</a>
+			</p>
+			<p>Lorem ipsum <a class="my-link-red" href="https://www.example.com">regular red link</a></p>
+			<p class="my-link-wrapper-with-underline">
+				Lorem ipsum <a class="my-link-red" href="https://www.example.com">underlined red link</a>
+			</p>
+		</div>
 	</section>
 </template>
 
@@ -39,6 +51,20 @@ a.my-custom-link-class {
 	.cdx-icon {
 		color: inherit;
 		margin-right: @spacing-x-small;
+	}
+}
+
+.my-link-wrapper {
+	a {
+		.cdx-mixin-link-base();
+
+		&.my-link-red {
+			.cdx-mixin-link-red();
+		}
+	}
+
+	&-with-underline a {
+		.cdx-mixin-link-underlined();
 	}
 }
 </style>
