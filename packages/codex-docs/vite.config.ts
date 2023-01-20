acfeb18439cd333c5134c0967fa574791bb667ea
Roan Kattouw <roan.kattouw@gmail.com>
2023-01-20 15:44:31 -0800

    build: Use shared Vite config for the demos build
    This has the effect of building separate LTR and RTL stylesheets for the
    demos build in the codex-docs package, as well as separate legacy and
    non-legacy builds.
    
    This demos build is only used in the VueTest extension.
    
    Also run the demos build as part of the overall build for the docs
    package, and remove the special "npm run build-demos" command in the
    docs package. VueTest can use "npm run doc" at the top level instead.
    This also builds the tokens and icons packages, without which the build
    doesn't work.
    
    Bug: T325560
    Change-Id: Ic3448ce8ffef47a1ba2b502cdabb4c100cfc1adb

--- packages/codex-docs/vite.config.ts
+++ packages/codex-docs/vite.config.ts
@@ -1,54 +1,24 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import { defineConfig, Alias } from 'vite';
+import codexBaseConfig from '../codex/vite-base.config';
 
-export default defineConfig( {
-	resolve: {
-		alias: [
-			{
-				// Use the experimental entry point so that work-in-progress (WIP) components are
-				// available.
-				find: '@wikimedia/codex',
-				replacement: '../codex/src/lib-wip.ts'
-			},
+export default defineConfig( ( env ) => {
+	const baseConfig = codexBaseConfig( env, {
+		libName: 'codex-demos'
+	} );
 
-			{
-				// Alias imports without /dist/ from codex-design-tokens to the dist/ directory.
-				find: /^@wikimedia\/codex-design-tokens\/([^/]+\.(less|css|scss|json))$/,
-				replacement: resolve( __dirname, '../codex-design-tokens/dist/$1' )
-			}
-		]
-	},
-
-	build: {
-		emptyOutDir: false,
-
-		lib: {
-			fileName: 'codex-demos',
-			name: 'codex-demos',
-			entry: 'src/lib.ts',
-			formats: [ 'es', 'cjs' ]
-		},
-
-		rollupOptions: {
-			output: {
-				entryFileNames: 'codex-demos.[format].js',
-				globals: {
-					vue: 'Vue'
+	return {
+		...baseConfig,
+		resolve: {
+			...baseConfig.resolve,
+			alias: [
+				...( baseConfig.resolve?.alias || [] ) as Alias[],
+				{
+					// Use the experimental entry point so that work-in-progress (WIP) components
+					// are available.
+					find: '@wikimedia/codex',
+					replacement: '../codex/src/lib-wip.ts'
 				}
-			},
-
-			// Codex is *not* externalized in this build, because we want to
-			// ensure that experimental components (which are not yet part
-			// of the published Codex release) can be used in demos.
-			external: [ 'vue' ]
-		},
-
-		target: [ 'es2015', 'edge18', 'safari11' ],
-		minify: true
-	},
-
-	plugins: [
-		vue()
-	]
+			]
+		}
+	};
 } );
