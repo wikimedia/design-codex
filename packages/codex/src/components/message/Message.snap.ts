e3a3f65b3923455e5ae8c8202ba0573719145e61
thiemowmde <thiemo.kreuz@wikimedia.de>
2023-04-14 17:44:07 +0200

    icons: Remove unnecessary code from .svg icon files
    I made sure they all look the same as before.
    
    Change-Id: I3b1aaa186af22b00d7de82ff3ee9a92ac0b4ce68

--- packages/codex/src/components/message/Message.snap.ts
+++ packages/codex/src/components/message/Message.snap.ts
@@ -180,9 +180,7 @@ exports[`matches the snapshot Case 4 success message: ({"type": "success"}) => H
         <!-- eslint-disable vue/no-v-html -->
         <g>
           <path
-            clip-rule="evenodd"
-            d="M10 20a10 10 0 100-20 10 10 0 000 20Zm-2-5 9-8.5L15.5 5 8 12 4.5 8.5 3 10l5 5Z"
-            fill-rule="evenodd"
+            d="M10 20a10 10 0 010-20 10 10 0 110 20Zm-2-5 9-8.5L15.5 5 8 12 4.5 8.5 3 10l5 5Z"
           />
         </g>
       </svg>
