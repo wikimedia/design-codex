ee5f2aeebf87aba9283d40c08b61fdc61aa7e2b9
Anne Tomasevich <atomasevich@wikimedia.org>
2023-04-27 14:50:50 -0400

    styles: Add mixins for flush layouts
    This patch adds a mixin for quiet buttons that sets a
    negative margin equal to the horizontal padding of the
    button, making the content of the button appear "flush"
    with the edge of the UI.
    
    We have chosen not to document this mixin because we
    would like to reevaluate the design decisions behind it
    and potentially create a new button variant (e.g. a
    quiet button with no background change on hover) or
    dropdown menu button component in the future.
    
    In a subsequent patch, we will add a media query for icon-only
    buttons that applies a different margin at different viewport
    widths.
    
    Bug: T322436
    Change-Id: Idf681b70119f4379dbbefa0f27d762971b667c8c

--- packages/codex/src/demo/ButtonDemo.vue
+++ packages/codex/src/demo/ButtonDemo.vue
@@ -79,13 +79,51 @@
 				</template>
 			</tbody>
 		</table>
+		<h3>Flush layout for buttons</h3>
+		<h4>Regular buttons</h4>
+		<div class="cdx-demo-flush-layout cdx-demo-flush-layout--regular">
+			<div class="cdx-demo-flush-layout__buttons">
+				<cdx-button weight="quiet">
+					Button one
+				</cdx-button>
+				<cdx-button weight="quiet">
+					Button two
+				</cdx-button>
+			</div>
+			<div class="cdx-demo-flush-layout__content">
+				<p>
+					Hemp seeds apple vinaigrette dark and stormy habanero golden coriander
+					peppermint asian pear frosted gingerbread bites Southern Italian almond milk
+					chai latte mint golden cayenne pepper.
+				</p>
+			</div>
+		</div>
+
+		<h4>Icon-only buttons</h4>
+		<div class="cdx-demo-flush-layout cdx-demo-flush-layout--icon-only">
+			<div class="cdx-demo-flush-layout__buttons">
+				<cdx-button weight="quiet" aria-label="Align left">
+					<cdx-icon :icon="cdxIconAlignLeft" />
+				</cdx-button>
+				<cdx-button weight="quiet" aria-label="Align right">
+					<cdx-icon :icon="cdxIconAlignRight" />
+				</cdx-button>
+			</div>
+			<div class="cdx-demo-flush-layout__content">
+				<p>
+					Hemp seeds apple vinaigrette dark and stormy habanero golden coriander
+					peppermint asian pear frosted gingerbread bites Southern Italian almond milk
+					chai latte mint golden cayenne pepper.
+				</p>
+			</div>
+		</div>
 	</section>
 </template>
 
 <script lang="ts" setup>
 import { CdxButton, CdxIcon } from '../lib';
 import { ButtonActions, ButtonWeights } from '../constants';
-import { cdxIconTrash } from '@wikimedia/codex-icons';
+import { cdxIconTrash, cdxIconAlignLeft, cdxIconAlignRight } from '@wikimedia/codex-icons';
 import getEventLogger from 'codex-docs/src/utils/getEventLogger';
 
 const onClick = getEventLogger<Event>( 'click' );
@@ -95,10 +133,44 @@ const onClick = getEventLogger<Event>( 'click' );
 // Note: you must import the design tokens before importing the css-icon mixin
 @import ( reference ) '@wikimedia/codex-design-tokens/theme-wikimedia-ui.less';
 @import ( reference ) '../themes/mixins/public/css-icon.less';
+@import ( reference ) '../themes/mixins/public/button-layout-flush.less';
 
 .cdx-demo-css-icon {
 	&--trash {
 		.cdx-mixin-css-icon( @cdx-icon-trash, @param-is-button-icon: true );
+	}
+}
+
+.cdx-demo-flush-layout {
+	&--regular {
+		.cdx-button:first-child {
+			.cdx-mixin-button-layout-flush( 'start' );
+		}
+
+		.cdx-button:last-child {
+			.cdx-mixin-button-layout-flush( 'end' );
+		}
+	}
+
+	&--icon-only {
+		.cdx-button:first-child {
+			.cdx-mixin-button-layout-flush( 'start', true );
+		}
+
+		.cdx-button:last-child {
+			.cdx-mixin-button-layout-flush( 'end', true );
+		}
+	}
+
+	&__buttons {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	&__content {
+		margin-top: @spacing-100;
+		border: @border-base;
+		padding: @spacing-100;
 	}
 }
 </style>
