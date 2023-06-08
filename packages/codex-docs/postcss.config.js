0ad576e97ddc258c261517fb69a23e9379866be8
Roan Kattouw <roan.kattouw@gmail.com>
2023-06-08 16:05:06 -0700

    docs: Apply rtlcss ([dir] selectors) to component demo examples
    This isn't necessary for RTL flipping, because these demos are never
    shown in RTL. However, many of them do contain CSS selectors intended to
    override selectors from Codex, and those overrides sometimes fail
    because the Codex selectors are prefixed with [dir] or [dir='ltr']. This
    change fixes those overrides by ensuring they are also prefixed the same
    way, so that the relative specificity between them isn't disturbed.
    
    Change-Id: Iad07cf9b8d0ea9636923ff3be5e2f61ce10c1e8f

--- packages/codex-docs/postcss.config.js
+++ packages/codex-docs/postcss.config.js
@@ -1,6 +1,7 @@
 /**
  * PostCSS config for the Codex docs site based on VitePress. This adds the rtlcss plugin, but
- * applies it only to files imported from the Codex package, not to files in the docs package.
+ * applies it only to files imported from the Codex package and to demo files, not to any other
+ * files in the docs package.
  *
  * Note this PostCSS config is ONLY used for building the VitePress docs site. It is NOT used for
  * building the 'codex-demos.*.css' files; those use 'postcss-base.config.js' from the Codex package
@@ -14,7 +15,11 @@ module.exports = {
 	/** @type {import('postcss').AcceptedPlugin[]} */
 	plugins: [
 		scoped( {
-			include: '../codex/**',
+			// Apply rtlcss  to the Codex components so that we can demonstrate them in LTR or RTL.
+			// The component demo examples are never shown in RTL, but they also need to have
+			// rtlcss applied to them so that their CSS selectors' specificity is enhanced to
+			// match the selectors from Codex they're overriding.
+			include: [ '../codex/**', 'component-demos/**/examples/**' ],
 			plugin: postcssRtlcss( {
 				useCalc: true,
 				processKeyFrames: true,
