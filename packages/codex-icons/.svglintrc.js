5eddfa95f31755835a2ccac603b8b85a323cc094
Volker E <volker.e@wikimedia.org>
2023-09-25 17:31:22 -0700

    build: Make svglint check for correct XML declaration
    Catch a common error in exported SVG files:
    Not adding unnecessary `standalone` attribute in XML declaration
    in icons, saving bytes across the board.
    Also disallowing it as wrong on `svg` element itself.
    
    Bug: T347445
    Change-Id: I4462ceef369bcfb9adf979bc5602c23e8343c612

--- packages/codex-icons/.svglintrc.js
+++ packages/codex-icons/.svglintrc.js
@@ -16,6 +16,8 @@ module.exports = {
 				'rule::selector': 'svg',
 				// Require xmlns to exist and be set to this value
 				xmlns: 'http://www.w3.org/2000/svg',
+				// Disallow `standalone` attribute, which would be wrong on `svg` anyway.
+				standalone: false,
 				// Require viewBox, width and height to be set to the standard 20x20 values
 				viewBox: '0 0 20 20',
 				width: '20',
@@ -35,17 +37,25 @@ module.exports = {
 		custom( reporter, $, ast ) {
 			// Require the first thing in the file to be an `<?xml ?>` declaration.
 			const documentNode = $[ 0 ];
-			if (
-				documentNode.firstChild &&
-				'name' in documentNode.firstChild &&
-				documentNode.firstChild.name !== '?xml'
-			) {
-				reporter.error(
-					'File must start with `<?xml ?>` declaration',
-					documentNode.firstChild,
-					ast
-				);
+			const firstChild = documentNode.firstChild;
+
+			if ( firstChild && firstChild.type === 'directive' && firstChild.name === '?xml' ) {
+				const versionMatch = /version="1.0"/.test( firstChild.data );
+				const encodingMatch = /encoding="UTF-8"/.test( firstChild.data );
+				const standaloneMatch = /standalone/.test( firstChild.data );
+
+				// `standalone` attribute is unnecessary and should be omitted.
+				if ( versionMatch && encodingMatch && !standaloneMatch ) {
+					// This is the desired XML declaration.
+					return;
+				}
 			}
+
+			reporter.error(
+				'File must start exactly with `<?xml version="1.0" encoding="UTF-8"?>` declaration',
+				firstChild || documentNode,
+				ast
+			);
 		}
 	}
 };
