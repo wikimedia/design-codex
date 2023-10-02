2f747aa45db898eacd76be0190f10b06f91e6d8b
Volker E <volker.e@wikimedia.org>
2023-10-02 14:32:32 -0700

    build: Add SVGO & svglint to 'codex-docs'
    This adds SVGO and svglint to the 'codex-docs' package build process.
    It also optimizes the SVGs in the 'docs/assets' directory, in preface
    of adding a huge number of SVGs for the component documentation, that
    are supposed to follow MediaWiki's coding conventions.
    
    Note that this is currently not requiring `title` elements as part of
    the documentation assets. They are always featured with `alt`
    attributes, we might add requirement for `title` post-MVP.
    
    Bug: T347906
    Change-Id: I6af55f5a515e7f9b4ebacb87dd5c5cf51101a871

---
+++ packages/codex-docs/.svglintrc.js
@@ -0,0 +1,51 @@
+// svglint doesn't have type defs, so improvise some here.
+/** @typedef {import('cheerio').Cheerio<import('cheerio').Document>} CheerioDocument */
+/** @typedef {import('cheerio').Node} CheerioNode */
+/** @typedef {{ error: ( msg: string, node: CheerioNode, ast: any ) => void }} Reporter */
+
+module.exports = {
+	rules: {
+		attr: [
+			{
+				// For the `<svg>` element
+				'rule::selector': 'svg',
+				// Require xmlns to exist and be set to this value
+				xmlns: 'http://www.w3.org/2000/svg',
+				// Disallow `standalone` attribute, which would be wrong on `svg` anyway.
+				standalone: false,
+				// Require viewBox, width and height to be set.
+				viewBox: true,
+				width: true,
+				height: true
+			}
+		],
+		/**
+		 * @param {Reporter} reporter
+		 * @param {CheerioDocument} $
+		 * @param {any} ast
+		 */
+		custom( reporter, $, ast ) {
+			// Require the first thing in the file to be an `<?xml ?>` declaration.
+			const documentNode = $[ 0 ];
+			const firstChild = documentNode.firstChild;
+
+			if ( firstChild && firstChild.type === 'directive' && firstChild.name === '?xml' ) {
+				const versionMatch = /version="1.0"/.test( firstChild.data );
+				const encodingMatch = /encoding="UTF-8"/.test( firstChild.data );
+				const standaloneMatch = /standalone/.test( firstChild.data );
+
+				// `standalone` attribute is unnecessary and should be omitted.
+				if ( versionMatch && encodingMatch && !standaloneMatch ) {
+					// This is the desired XML declaration.
+					return;
+				}
+			}
+
+			reporter.error(
+				'File must start exactly with `<?xml version="1.0" encoding="UTF-8"?>` declaration',
+				firstChild || documentNode,
+				ast
+			);
+		}
+	}
+};
