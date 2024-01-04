8786ed31f0744259b700fdf2cb1273d65832a979
Roan Kattouw <roan.kattouw@gmail.com>
2024-01-04 15:55:01 -0800

    docs: Redirect from old "adding icons" URL to the new one
    VitePress doesn't support redirects natively, but the router API allows
    us to set up a crude DIY redirect system. Do this, and start a list of
    redirects that's currently just the "adding icons" page.
    
    Bug: T312195
    Change-Id: I3456600e6b8b100c9cf40625e62c0bc604ade0f8

--- packages/codex-docs/docs/.vitepress/theme/index.ts
+++ packages/codex-docs/docs/.vitepress/theme/index.ts
@@ -11,13 +11,31 @@ import './custom.css';
 // Import special fixes for syntax highlighting colors.
 import './syntax-highlighting-fixes.css';
 
+const redirects: Record<string, string> = {
+	// The "adding new icons" page was moved from icons to contributing code.
+	'/icons/adding-new.html': '/contributing/contributing-code/adding-new-icons.html'
+};
+
 const customTheme: Theme = {
 	...DefaultTheme,
 	Layout: CustomLayout,
-	enhanceApp( { app } ) {
+	enhanceApp( { app, router } ) {
 		app.component( 'CdxDemoWrapper', CdxDemoWrapper );
 		app.component( 'CdxDemoSlotIcon', CdxDemoSlotIcon );
 		app.component( 'CdxDemoRules', CdxDemoRules );
+
+		// If we went to a URL that matches a redirect entry, go to the redirected URL instead.
+		router.onAfterRouteChanged = async ( to ) => {
+			for ( const [ redirFrom, redirTo ] of Object.entries( redirects ) ) {
+				if ( to.endsWith( redirFrom ) ) {
+					// router.go() wants a full URL with domain name etc. Instead of trying to
+					// construct that ourselves, obtain it by doing a replace on `to` (which is
+					// also a full URL).
+					await router.go( to.replace( redirFrom, redirTo ) );
+					break;
+				}
+			}
+		};
 	}
 };
 
