4091e2cbcb265b91c70a5c42cc1e6a75a7af8118
Roan Kattouw <roan.kattouw@gmail.com>
2024-04-26 11:57:48 -0700

    demo: Add a framed toggle to the Tabs sandbox
    Change-Id: I778cf7cdc529c32531d8565519c4e276178a6b43

--- packages/codex/src/demo/TabsDemo.vue
+++ packages/codex/src/demo/TabsDemo.vue
@@ -2,7 +2,7 @@
 	<section id="cdx-tabs">
 		<h2>Tabs</h2>
 
-		<cdx-tabs v-model:active="activeTab">
+		<cdx-tabs v-model:active="activeTab" :framed="framed">
 			<cdx-tab
 				v-for="( tab, index ) in tabs"
 				:key="index"
@@ -15,14 +15,20 @@
 		<cdx-button @click="reverseTabs">
 			Reverse tab order
 		</cdx-button>
+		<cdx-field>
+			<cdx-toggle-switch v-model="framed">
+				Use framed tabs
+			</cdx-toggle-switch>
+		</cdx-field>
 	</section>
 </template>
 
 <script lang="ts" setup>
-import { CdxButton, CdxTab, CdxTabs } from '../lib';
+import { CdxButton, CdxToggleSwitch, CdxField, CdxTab, CdxTabs } from '../lib';
 import { ref } from 'vue';
 const activeTab = ref( 'tab1' );
 const tabs = ref( [ 'tab1', 'tab2', 'tab3' ] );
+const framed = ref( false );
 
 function reverseTabs() {
 	tabs.value.reverse();
