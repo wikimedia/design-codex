e1513ebc39db1892a75f257f27bba15695cadf3e
Roan Kattouw <roan.kattouw@gmail.com>
2024-12-19 12:53:11 -0500

    tests: Add a test for expandDeprecationMessage
    Change-Id: Ifd4f1a7a7f240da82cf2393cdb4a46cae5e69b21

--- packages/codex-docs/src/utils/tokens.test.ts
+++ packages/codex-docs/src/utils/tokens.test.ts
@@ -1,18 +1,19 @@
-import { flattenDesignTokensTree } from './tokens';
+import { expandDeprecationMessage, flattenDesignTokensTree } from './tokens';
 import { DesignToken, DesignTokensTree } from '../types';
+
+const baseTokenData = {
+	filePath: 'src/codex-base.json',
+	original: {
+		value: ''
+	},
+	attributes: {
+		tokens: []
+	},
+	path: []
+};
 
 describe( 'flattenDesignTokensTree', () => {
 	it( 'flattens properly', () => {
-		const baseTokenData = {
-			filePath: 'src/codex-base.json',
-			original: {
-				value: ''
-			},
-			attributes: {
-				tokens: []
-			},
-			path: []
-		};
 		// A few demo tokens based on design tokens package
 		const exampleToken: DesignToken = {
 			...baseTokenData,
@@ -74,5 +75,94 @@ describe( 'flattenDesignTokensTree', () => {
 		expect( flattenDesignTokensTree( treeWithComment, [ 'nestedTree' ] ) ).toMatchObject( [
 			exampleToken
 		] );
+	} );
+} );
+
+describe( 'expandDeprecationMessage', () => {
+	it( 'does not modify non-deprecated tokens', () => {
+		const regularToken: DesignToken = {
+			...baseTokenData,
+			name: 'foo-bar',
+			value: 'baz',
+			original: {
+				value: 'baz'
+			},
+			attributes: {
+				tokens: []
+			}
+		};
+		expect( expandDeprecationMessage( regularToken ) ).toEqual( regularToken );
+	} );
+
+	it( 'does not modify deprecated tokens that do not use another token', () => {
+		const deprecatedTokenWithoutReference: DesignToken = {
+			...baseTokenData,
+			name: 'old-foo-bar',
+			value: 'quux',
+			original: {
+				value: 'quux'
+			},
+			attributes: {
+				tokens: []
+			},
+			deprecated: 'You should use foo-bar now'
+		};
+		expect( expandDeprecationMessage( deprecatedTokenWithoutReference ) )
+			.toEqual( deprecatedTokenWithoutReference );
+	} );
+
+	it( 'does not modify deprecated tokens that use multiple other tokens', () => {
+		const deprecatedTokenWithMultiRefs: DesignToken = {
+			...baseTokenData,
+			name: 'old-foo-bar',
+			value: '1px 2px',
+			original: {
+				value: '{ foo.bar } { foo.baz }'
+			},
+			attributes: {
+				tokens: [ 'foo.bar', 'foo.baz' ]
+			},
+			deprecated: true
+		};
+		expect( expandDeprecationMessage( deprecatedTokenWithMultiRefs ) )
+			.toEqual( deprecatedTokenWithMultiRefs );
+	} );
+
+	it( 'generates a deprecation message for tokens that use one other token', () => {
+		const deprecatedTokenWithRef: DesignToken = {
+			...baseTokenData,
+			name: 'old-foo-bar',
+			value: '1px',
+			original: {
+				value: '{ foo.bar }'
+			},
+			attributes: {
+				tokens: [ 'foo.bar' ]
+			},
+			deprecated: true
+		};
+		expect( expandDeprecationMessage( deprecatedTokenWithRef ) ).toEqual( {
+			...deprecatedTokenWithRef,
+			deprecated: 'Use `foo-bar` instead.'
+		} );
+	} );
+
+	it( 'appends to the existing deprecation message if there is one', () => {
+		const deprecatedTokenWithMessage: DesignToken = {
+			...baseTokenData,
+			name: 'old-foo-bar',
+			value: '1px',
+			original: {
+				value: '{ foo.bar }'
+			},
+			attributes: {
+				tokens: [ 'foo.bar' ]
+			},
+			deprecated: 'This is old'
+		};
+		expect( expandDeprecationMessage( deprecatedTokenWithMessage ) ).toEqual( {
+			...deprecatedTokenWithMessage,
+			deprecated: 'This is old (use `foo-bar` instead)'
+		} );
 	} );
 } );
