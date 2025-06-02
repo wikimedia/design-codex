ed2c6613077e3f8ab44c06589191c0b358c9fcca
Anne Tomasevich <atomasevich@wikimedia.org>
2025-06-02 09:05:21 -0400

    tests: Update tests for composables
    - Nest all unit tests in a describe block under the composable
      name
    
    Bug: T308520
    Change-Id: I14609a5e886dcc59bb09128fa5d35922290a5498

--- packages/codex/src/composables/useGeneratedId.test.ts
+++ packages/codex/src/composables/useGeneratedId.test.ts
@@ -1,182 +1,186 @@
 import { mount, shallowMount } from '@vue/test-utils';
 import useGeneratedId from './useGeneratedId';
 
-describe( 'When called without an identifier argument', () => {
-	describe( 'When parent component has no "id" specified', () => {
-		const Component = {
-			template: '<div :id="id">Test</div>',
-			setup() {
-				const id = useGeneratedId();
-				return { id };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component );
-			expect( typeof wrapper.vm.id ).toBe( 'string' );
-		} );
-		it( 'prefixes the generated id with "cdx-"', () => {
-			const wrapper = shallowMount( Component );
-			expect( wrapper.vm.id ).toMatch( /^cdx-/ );
-		} );
-		it( 'ensures multiple usages do not cause collisions', () => {
-			const wrapper1 = shallowMount( Component );
-			const wrapper2 = shallowMount( Component );
-			expect( wrapper1.vm.id ).not.toEqual( wrapper2.vm.id );
-		} );
-		it( 'increments the counter each time it is called and appends that value as a suffix', () => {
-			const wrapper1 = shallowMount( Component );
-			const wrapper2 = shallowMount( Component );
-			function getSuffix( id: string ) {
-				return Number( id.split( '-' ).slice( -1 ) );
-			}
-			expect( getSuffix( wrapper2.vm.id ) ).toBeGreaterThan( getSuffix( wrapper1.vm.id ) );
-		} );
-	} );
-	describe( 'When parent component has an ID specified as an attribute', () => {
-		const Component = {
-			template: '<div :id="generatedId">Test</div>',
-			setup() {
-				const generatedId = useGeneratedId();
-				return { generatedId };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component, { attrs: { id: 'foo' } } );
-			expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
-		} );
-		it( 'includes the existing ID', () => {
-			const wrapper = mount( Component, { attrs: { id: 'foo' } } );
-			expect( wrapper.vm.generatedId ).toMatch( /^cdx-foo/ );
-		} );
-	} );
-	describe( 'When parent component has an ID specified as a prop', () => {
-		const Component = {
-			template: '<div :id="generatedId">Test</div>',
-			setup() {
-				const generatedId = useGeneratedId();
-				return { generatedId };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component, { props: { id: 'bar' } } );
-			expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
-		} );
-		it( 'includes the existing ID', () => {
-			const wrapper = mount( Component, { props: { id: 'bar' } } );
-			expect( wrapper.vm.generatedId ).toMatch( /^cdx-bar/ );
-		} );
-	} );
-	describe( 'When parent component has "id" as both a prop and an attribute', () => {
-		const Component = {
-			template: '<div :id="generatedId">Test</div>',
-			setup() {
-				const generatedId = useGeneratedId();
-				return { generatedId };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component, {
-				attrs: { id: 'foo' },
-				props: { id: 'bar' }
+describe( 'useGeneratedId', () => {
+	describe( 'when called without an identifier argument', () => {
+		describe( 'and parent component has no "id" specified', () => {
+			const Component = {
+				template: '<div :id="id">Test</div>',
+				setup() {
+					const id = useGeneratedId();
+					return { id };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component );
+				expect( typeof wrapper.vm.id ).toBe( 'string' );
 			} );
-			expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
-		} );
-		it( 'prefers the prop over the attribute', () => {
-			const wrapper = mount( Component, {
-				attrs: { id: 'foo' },
-				props: { id: 'bar' }
+			it( 'prefixes the generated id with "cdx-"', () => {
+				const wrapper = shallowMount( Component );
+				expect( wrapper.vm.id ).toMatch( /^cdx-/ );
 			} );
-			expect( wrapper.vm.generatedId ).toMatch( /^cdx-bar/ );
-		} );
-	} );
-} );
-describe( 'When called with an identifer argument', () => {
-	const identifier = 'baz';
-	describe( 'When parent component has no "id" specified', () => {
-		const Component = {
-			template: '<div :id="id">Test</div>',
-			setup() {
-				const id = useGeneratedId( identifier );
-				return { id };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component );
-			expect( typeof wrapper.vm.id ).toBe( 'string' );
-		} );
-		it( 'returns a string which includes the provided identifier', () => {
-			const wrapper = shallowMount( Component );
-			expect( wrapper.vm.id ).toMatch( /^cdx-baz/ );
-		} );
-		it( 'ensures multiple usages do not cause collisions', () => {
-			const wrapper1 = shallowMount( Component );
-			const wrapper2 = shallowMount( Component );
-			expect( wrapper1.vm.id ).not.toEqual( wrapper2.vm.id );
-		} );
-		it( 'increments the counter each time it is called and appends that value as a suffix', () => {
-			const wrapper1 = shallowMount( Component );
-			const wrapper2 = shallowMount( Component );
-			function getSuffix( id: string ) {
-				return Number( id.split( '-' ).slice( -1 ) );
-			}
-			expect( getSuffix( wrapper2.vm.id ) ).toBeGreaterThan( getSuffix( wrapper1.vm.id ) );
-		} );
-	} );
-	describe( 'When parent component has an ID specified as an attribute', () => {
-		const Component = {
-			template: '<div :id="generatedId">Test</div>',
-			setup() {
-				const generatedId = useGeneratedId( identifier );
-				return { generatedId };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component, { attrs: { id: 'foo' } } );
-			expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
-		} );
-		it( 'prefers the identifier argument over the attribute', () => {
-			const wrapper = mount( Component, { attrs: { id: 'foo' } } );
-			expect( wrapper.vm.generatedId ).toMatch( /^cdx-baz/ );
-		} );
-	} );
-	describe( 'When parent component has an ID specified as a prop', () => {
-		const Component = {
-			template: '<div :id="generatedId">Test</div>',
-			setup() {
-				const generatedId = useGeneratedId( identifier );
-				return { generatedId };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component, { props: { id: 'bar' } } );
-			expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
-		} );
-		it( 'prefers the identifier argument over the prop', () => {
-			const wrapper = mount( Component, { props: { id: 'bar' } } );
-			expect( wrapper.vm.generatedId ).toMatch( /^cdx-baz/ );
-		} );
-	} );
-	describe( 'When parent component has "id" as both a prop and an attribute', () => {
-		const Component = {
-			template: '<div :id="generatedId">Test</div>',
-			setup() {
-				const generatedId = useGeneratedId( identifier );
-				return { generatedId };
-			}
-		};
-		it( 'returns a string', () => {
-			const wrapper = shallowMount( Component, {
-				attrs: { id: 'foo' },
-				props: { id: 'bar' }
+			it( 'ensures multiple usages do not cause collisions', () => {
+				const wrapper1 = shallowMount( Component );
+				const wrapper2 = shallowMount( Component );
+				expect( wrapper1.vm.id ).not.toEqual( wrapper2.vm.id );
 			} );
-			expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
-		} );
-		it( 'prefers the identifier to both prop and attribute', () => {
-			const wrapper = mount( Component, {
-				attrs: { id: 'foo' },
-				props: { id: 'bar' }
+			it( 'increments the counter each time it is called and appends that value as a suffix', () => {
+				const wrapper1 = shallowMount( Component );
+				const wrapper2 = shallowMount( Component );
+				function getSuffix( id: string ) {
+					return Number( id.split( '-' ).slice( -1 ) );
+				}
+				expect( getSuffix( wrapper2.vm.id ) ).toBeGreaterThan(
+					getSuffix( wrapper1.vm.id ) );
 			} );
-			expect( wrapper.vm.generatedId ).toMatch( /^cdx-baz/ );
+		} );
+		describe( 'when parent component has an ID specified as an attribute', () => {
+			const Component = {
+				template: '<div :id="generatedId">Test</div>',
+				setup() {
+					const generatedId = useGeneratedId();
+					return { generatedId };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component, { attrs: { id: 'foo' } } );
+				expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
+			} );
+			it( 'includes the existing ID', () => {
+				const wrapper = mount( Component, { attrs: { id: 'foo' } } );
+				expect( wrapper.vm.generatedId ).toMatch( /^cdx-foo/ );
+			} );
+		} );
+		describe( 'when parent component has an ID specified as a prop', () => {
+			const Component = {
+				template: '<div :id="generatedId">Test</div>',
+				setup() {
+					const generatedId = useGeneratedId();
+					return { generatedId };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component, { props: { id: 'bar' } } );
+				expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
+			} );
+			it( 'includes the existing ID', () => {
+				const wrapper = mount( Component, { props: { id: 'bar' } } );
+				expect( wrapper.vm.generatedId ).toMatch( /^cdx-bar/ );
+			} );
+		} );
+		describe( 'when parent component has "id" as both a prop and an attribute', () => {
+			const Component = {
+				template: '<div :id="generatedId">Test</div>',
+				setup() {
+					const generatedId = useGeneratedId();
+					return { generatedId };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component, {
+					attrs: { id: 'foo' },
+					props: { id: 'bar' }
+				} );
+				expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
+			} );
+			it( 'prefers the prop over the attribute', () => {
+				const wrapper = mount( Component, {
+					attrs: { id: 'foo' },
+					props: { id: 'bar' }
+				} );
+				expect( wrapper.vm.generatedId ).toMatch( /^cdx-bar/ );
+			} );
+		} );
+	} );
+	describe( 'when called with an identifier argument', () => {
+		const identifier = 'baz';
+		describe( 'When parent component has no "id" specified', () => {
+			const Component = {
+				template: '<div :id="id">Test</div>',
+				setup() {
+					const id = useGeneratedId( identifier );
+					return { id };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component );
+				expect( typeof wrapper.vm.id ).toBe( 'string' );
+			} );
+			it( 'returns a string which includes the provided identifier', () => {
+				const wrapper = shallowMount( Component );
+				expect( wrapper.vm.id ).toMatch( /^cdx-baz/ );
+			} );
+			it( 'ensures multiple usages do not cause collisions', () => {
+				const wrapper1 = shallowMount( Component );
+				const wrapper2 = shallowMount( Component );
+				expect( wrapper1.vm.id ).not.toEqual( wrapper2.vm.id );
+			} );
+			it( 'increments the counter each time it is called and appends that value as a suffix', () => {
+				const wrapper1 = shallowMount( Component );
+				const wrapper2 = shallowMount( Component );
+				function getSuffix( id: string ) {
+					return Number( id.split( '-' ).slice( -1 ) );
+				}
+				expect( getSuffix( wrapper2.vm.id ) ).toBeGreaterThan(
+					getSuffix( wrapper1.vm.id ) );
+			} );
+		} );
+		describe( 'when parent component has an ID specified as an attribute', () => {
+			const Component = {
+				template: '<div :id="generatedId">Test</div>',
+				setup() {
+					const generatedId = useGeneratedId( identifier );
+					return { generatedId };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component, { attrs: { id: 'foo' } } );
+				expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
+			} );
+			it( 'prefers the identifier argument over the attribute', () => {
+				const wrapper = mount( Component, { attrs: { id: 'foo' } } );
+				expect( wrapper.vm.generatedId ).toMatch( /^cdx-baz/ );
+			} );
+		} );
+		describe( 'when parent component has an ID specified as a prop', () => {
+			const Component = {
+				template: '<div :id="generatedId">Test</div>',
+				setup() {
+					const generatedId = useGeneratedId( identifier );
+					return { generatedId };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component, { props: { id: 'bar' } } );
+				expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
+			} );
+			it( 'prefers the identifier argument over the prop', () => {
+				const wrapper = mount( Component, { props: { id: 'bar' } } );
+				expect( wrapper.vm.generatedId ).toMatch( /^cdx-baz/ );
+			} );
+		} );
+		describe( 'when parent component has "id" as both a prop and an attribute', () => {
+			const Component = {
+				template: '<div :id="generatedId">Test</div>',
+				setup() {
+					const generatedId = useGeneratedId( identifier );
+					return { generatedId };
+				}
+			};
+			it( 'returns a string', () => {
+				const wrapper = shallowMount( Component, {
+					attrs: { id: 'foo' },
+					props: { id: 'bar' }
+				} );
+				expect( typeof wrapper.vm.generatedId ).toBe( 'string' );
+			} );
+			it( 'prefers the identifier to both prop and attribute', () => {
+				const wrapper = mount( Component, {
+					attrs: { id: 'foo' },
+					props: { id: 'bar' }
+				} );
+				expect( wrapper.vm.generatedId ).toMatch( /^cdx-baz/ );
+			} );
 		} );
 	} );
 } );
--- packages/codex/src/composables/useSplitAttributes.test.ts
+++ packages/codex/src/composables/useSplitAttributes.test.ts
@@ -9,58 +9,61 @@ const internalClasses = computed( () => ( {
 	'class--modifier-2': false
 } ) );
 
-describe( 'When called without class or style attributes', () => {
-	it( 'returns empty object for rootClasses', () => {
-		const { rootClasses } = useSplitAttributes( attrs );
-		expect( rootClasses.value ).toEqual( {} );
+describe( 'useSplitAttributes', () => {
+	describe( 'when called without class or style attributes', () => {
+		it( 'returns empty object for rootClasses', () => {
+			const { rootClasses } = useSplitAttributes( attrs );
+			expect( rootClasses.value ).toEqual( {} );
+		} );
+
+		it( 'returns undefined for rootStyle', () => {
+			const { rootStyle } = useSplitAttributes( attrs );
+			expect( rootStyle.value ).toEqual( undefined );
+		} );
+
+		it( 'returns attrs as-is for otherAttrs', () => {
+			const { otherAttrs } = useSplitAttributes( attrs );
+			expect( otherAttrs.value ).toEqual( attrs );
+		} );
 	} );
 
-	it( 'returns undefined for rootStyle', () => {
-		const { rootStyle } = useSplitAttributes( attrs );
-		expect( rootStyle.value ).toEqual( undefined );
+	describe( 'when called with a class attribute', () => {
+		it( 'properly adds provided classes to rootClasses', () => {
+			const { rootClasses } = useSplitAttributes( attrsWithClass );
+			const expected = {
+				'class-1': true,
+				'class-2': true
+			};
+			expect( rootClasses.value ).toEqual( expected );
+		} );
+
+		it( 'properly adds internal classes to rootClasses', () => {
+			const { rootClasses } = useSplitAttributes( attrsWithClass, internalClasses );
+			const expected = {
+				'class--modifier-1': true,
+				'class--modifier-2': false,
+				'class-1': true,
+				'class-2': true
+			};
+			expect( rootClasses.value ).toEqual( expected );
+		} );
+
+		it( 'returns otherAttrs with class removed', () => {
+			const { otherAttrs } = useSplitAttributes( attrsWithClass );
+			expect( otherAttrs.value ).toEqual( attrs );
+		} );
 	} );
 
-	it( 'returns attrs as-is for otherAttrs', () => {
-		const { otherAttrs } = useSplitAttributes( attrs );
-		expect( otherAttrs.value ).toEqual( attrs );
-	} );
-} );
+	describe( 'when called with a style attribute', () => {
+		it( 'returns rootStyle object', () => {
+			const { rootStyle } = useSplitAttributes( attrsWithStyle );
+			expect( rootStyle.value ).toEqual( { 'font-weight': 'bold' } );
+		} );
 
-describe( 'When called with a class attribute', () => {
-	it( 'properly adds provided classes to rootClasses', () => {
-		const { rootClasses } = useSplitAttributes( attrsWithClass );
-		const expected = {
-			'class-1': true,
-			'class-2': true
-		};
-		expect( rootClasses.value ).toEqual( expected );
+		it( 'returns otherAttrs with class removed', () => {
+			const { otherAttrs } = useSplitAttributes( attrsWithStyle );
+			expect( otherAttrs.value ).toEqual( attrs );
+		} );
 	} );
 
-	it( 'properly adds internal classes to rootClasses', () => {
-		const { rootClasses } = useSplitAttributes( attrsWithClass, internalClasses );
-		const expected = {
-			'class--modifier-1': true,
-			'class--modifier-2': false,
-			'class-1': true,
-			'class-2': true
-		};
-		expect( rootClasses.value ).toEqual( expected );
-	} );
-
-	it( 'returns otherAttrs with class removed', () => {
-		const { otherAttrs } = useSplitAttributes( attrsWithClass );
-		expect( otherAttrs.value ).toEqual( attrs );
-	} );
-} );
-
-describe( 'When called with a style attribute', () => {
-	it( 'returns rootStyle object', () => {
-		const { rootStyle } = useSplitAttributes( attrsWithStyle );
-		expect( rootStyle.value ).toEqual( { 'font-weight': 'bold' } );
-	} );
-
-	it( 'returns otherAttrs with class removed', () => {
-		const { otherAttrs } = useSplitAttributes( attrsWithStyle );
-		expect( otherAttrs.value ).toEqual( attrs );
-	} );
 } );
